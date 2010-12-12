c168b542f29a449e85cd8ad73f04b265f3fe8d00
indelay <indelay@gmail.com>
2010-12-13 01:22:39 +0300

    shiva new IPC for bus

---
+++ bus_ipc.h
@@ -0,0 +1,22 @@
+#include <inttypes.h>
+#include <sys/msg.h>
+//==============================================================================
+// messages from bus
+typedef struct{
+  long    type;   //must be long (used by filter)
+  int     cnt;
+  uint8_t data[1024];
+}_bus_msg;
+#define _bus_msg_size     (sizeof(_bus_msg)-offsetof(_bus_msg,cnt))
+#define _bus_msg_size_nd  (offsetof(_bus_msg,data)-offsetof(_bus_msg,cnt))
+enum{
+  mtypeBusAll=0,  //reserved for receive all filter
+  mtypeBusTx,     //transmit to uart bus
+  mtypeBusRx,     //received from uart bus
+  mtypeUdpTx,     //transmit to UDP
+//  mtypeUdpRx,   //received from UDP
+};
+//------------------------------------------------------------------------------
+static inline int getMsgId(void){return msgget(ftok("/etc/fstab",'a'),0644|IPC_CREAT);};
+//==============================================================================
+
