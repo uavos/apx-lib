012b10cddc24b240cbb0d236a3801e8f58803cff
Aliaksei Stratsilatau <sa@uavos.com>
2011-07-16 21:07:06 +0300

    AP7: uart loader, no RESET.

--- bus_defaults.h
+++ bus_defaults.h
@@ -1,11 +1,13 @@
 #ifndef _BUS_DEFAULTS_H_
 #define _BUS_DEFAULTS_H_
+#include <inttypes.h>
 //=============================================================================
 #define BUS_MAX_PACKET          1024
 //=============================================================================
 //default bus commands (service packets)
 enum{
   apc_ACK=0,
+  apc_ID,       //return _node_id
   apc_Info,     //return _node_info
   apc_SetAdr,   //set new address[_node_id] dtype and FUID used to filter
 
@@ -20,25 +22,52 @@ enum{
 };
 //=============================================================================
 // node configuration. saved in EEPROM
+typedef uint8_t _node_sn[12]; //chip serial number
 typedef struct{
-  unsigned char dtype;          //device type (stored in flash @end of page 0)
-  unsigned char FUID[3];        //device serial number (set during loader prg)
-  unsigned char dadr;           //device address (dynamically assigned)
-  char          name[16];       //device name string
-}__attribute__((packed)) _node_id;
+  _node_sn      sn;
+  uint32_t      mcu_id;         //MCU DEV_ID code
+  uint8_t       dadr;           //device address (dynamically assigned)
+  uint8_t       name[16];       //device name string
+}__attribute__((packed)) _node_id; //filled by hwInit;
 //-------------------------------------------------
 typedef struct {
-  _node_id      id;
+  _node_id  id;
   struct{             //general ports availability (dynamically set by drivers)
-    unsigned char ctr;      //configurable controls
-    unsigned char pwm;      //pwm servo outputs
-    unsigned char out;      //logical outputs
-    unsigned char inp;      //logical inputs
-    unsigned char adc;      //analog inputs
+    uint8_t ctr;      //configurable controls
+    uint8_t pwm;      //pwm servo outputs
+    uint8_t out;      //logical outputs
+    uint8_t inp;      //logical inputs
+    uint8_t adc;      //analog inputs
   }ports;
-  unsigned char vars[32];   //bitmask of known variables
+  uint8_t vars[32];   //bitmask of known variables
 }__attribute__((packed)) _node_info;
 //=============================================================================
+//=============================================================================
+// Loader packet: [ALL PACKETS ARE BROADCAST SRV - src=0x80]
+// hdr: src=0x80 (srv broadcast), cmd=apc_loader
+// <0x80>,<apc_loader=0xFF>,<ldc_XXX>,<_node_sn>,<data...>
+// to enter loader:
+// <0x80>,<apc_loader=0xFF>,<ldc_init>,<_node_sn>
+// loader filter packets by _node_sn
+// loader must reply to standard command 'apc_ID'
+//------------------------------------------------------------------------------
+typedef enum {
+  ldc_init=0,   // start loader, re: <_ldc_file> =flash size
+  ldc_file,     // set file info <_ldc_file>, re: <_ldc_file>
+  ldc_write,    // write file data <_ldc_write>, re: <_ldc_write::hdr>
+} _ldr_cmd;
+//loader data
+typedef struct{
+  uint32_t      start_address;
+  uint16_t      size;        // flash size in kB
+}__attribute__((packed)) _ldc_file;
+typedef struct{
+  struct{
+    uint32_t    start_address;
+    uint16_t    data_size;
+  }__attribute__((packed)) hdr;
+  uint8_t       data[256];
+}__attribute__((packed)) _ldc_write;
 //=============================================================================
 #endif
 
