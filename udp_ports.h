c54c2006a6d5bdc6fa7c7e37b213d868fc0818d9
Aliaksei Stratsilatau <sa@uavos.com>
2012-05-30 20:14:03 +0300

    X-plane display via UDP.

--- udp_ports.h
+++ udp_ports.h
@@ -3,9 +3,9 @@
 //==============================================================================
 #define UDP_MODEM_TX            9010    //modem tx (send to UAV)
 
-
 #define UDP_PORT_TELEMTRY       9000    //shiva: telemetry downlink/uplink
 #define UDP_PORT_SIM            9002    //simulator: imu/servo
+#define UDP_PORT_DSP            9004    //simulator: display
 
 #define UDP_HOST_GCU            "10.1.1.100"    //machine where 'GCU' runs
 #define UDP_HOST_UAV            "10.1.1.1"      //machine where 'shiva' runs
