d1763d56ba74a1aae8bbd762f83e46a3660befd8
Aliaksei Stratsilatau <sa@uavos.com>
2015-10-25 16:28:40 +0300

    new mhx firmware. xplane-sil update.

--- tcp_ports.h
+++ tcp_ports.h
@@ -4,18 +4,5 @@
 #define TCP_PORT_SERVER         9080    //http server
 #define TCP_PORT_SIM            9088    //simulator: imu/servo
 #define UDP_PORT_DISCOVER       9000    //discover service
-
-
-//#define UDP_MODEM_TX            9010    //modem tx (send to UAV)
-//#define UDP_MODEM_CMD           9030    //modem command string (send to UAV)
-
-//#define UDP_PORT_TELEMTRY       9000    //shiva: telemetry downlink/uplink
-//#define UDP_PORT_SIM            9002    //simulator: imu/servo
-//#define UDP_PORT_DSP            9004    //simulator: display
-//#define UDP_PORT_HIL            9006    //simulator: hil
-
-//#define UDP_HOST_GCU            "10.1.1.100"    //machine where 'GCU' runs
-//#define UDP_HOST_UAV            "10.1.1.1"      //machine where 'shiva' runs
-//#define UDP_HOST_SIM            "10.1.1.101"    //machine where 'xplane' runs
 //==============================================================================
 #endif
