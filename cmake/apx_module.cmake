fef6fbe758bdc94720e4fc57e8f4150cce61097a
Aliaksei Stratsilatau <sa@uavos.com>
2020-12-16 15:31:52 +0300

    module generic features

--- cmake/apx_module.cmake
+++ cmake/apx_module.cmake
@@ -1,43 +1,65 @@
+macro(module_srcs)
+    if(NOT SRCS)
+        if(INTERFACE)
+            set(SRCS "*.h*")
+        else()
+            set(SRCS "*.[ch]*")
+        endif()
+    endif()
+    set(srcs)
+    foreach(src ${SRCS})
+        if(NOT EXISTS ${src})
+            file(
+                GLOB src_exp
+                RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}
+                ${src}
+            )
+            if(src_exp)
+                set(src ${src_exp})
+            endif()
+        endif()
+        if(NOT src MATCHES "[\*]")
+            list(APPEND srcs ${src})
+        endif()
+    endforeach()
+    set(SRCS ${srcs})
+endmacro()
+
 # cmake-format: off
 function(apx_module)
 
     apx_parse_function_args(
         NAME apx_module
         ONE_VALUE
-            MODULE
+            MODULE_NAME
             PREFIX
         MULTI_VALUE
-            SRCS
             INCLUDES
+            SRCS
             DEPENDS
-            COMPILE_FLAGS
-            LINK_FLAGS
             GENSRC
         OPTIONS
-            INTERFACE
             INIT
         ARGN ${ARGN}
     )
 # cmake-format: on
 
-    if(NOT MODULE)
-        # guess module name
-        set(path ${CMAKE_CURRENT_SOURCE_DIR})
-        if(${path} MATCHES "^${APX_SHARED_DIR}")
-            file(RELATIVE_PATH path ${APX_SHARED_DIR} ${path})
-            set(path "shared.${path}")
-            get_filename_component(GROUP_DIR ${APX_SHARED_DIR} ABSOLUTE)
-        elseif(${path} MATCHES "^${APX_MODULES_DIR}")
-            file(RELATIVE_PATH path ${APX_MODULES_DIR} ${path})
-            get_filename_component(GROUP_DIR ${APX_SHARED_DIR} ABSOLUTE)
-        else()
-            message(FATAL_ERROR "Can't guess module name: ${CMAKE_CURRENT_SOURCE_DIR}")
-        endif()
-        string(REPLACE "/" "." MODULE ${path})
+    # guess module name
+
+    set(path ${CMAKE_CURRENT_SOURCE_DIR})
+    if(${path} MATCHES "^${APX_SHARED_DIR}")
+        file(RELATIVE_PATH path ${APX_SHARED_DIR} ${path})
+        set(path "shared.${path}")
+    elseif(${path} MATCHES "^${APX_MODULES_DIR}")
+        file(RELATIVE_PATH path ${APX_MODULES_DIR} ${path})
+    else()
+        message(FATAL_ERROR "Can't guess module name: ${CMAKE_CURRENT_SOURCE_DIR}")
     endif()
-    string(REPLACE "." ";" MODULE_NAME ${MODULE})
-    list(GET MODULE_NAME -1 MODULE_NAME)
-    string(TOLOWER ${MODULE_NAME} MODULE_NAME_LC)
+    string(REPLACE "/" "." MODULE ${path})
+    if(NOT MODULE_NAME)
+        string(REPLACE "." ";" MODULE_NAME ${MODULE})
+        list(GET MODULE_NAME -1 MODULE_NAME)
+    endif()
 
     # first include dependencies (other modules)
     if(DEPENDS)
@@ -48,33 +70,62 @@ function(apx_module)
 
     # collect global modules list properties
     # add this module to the list *after* all dependencies for natural sorting
-    set_property(GLOBAL APPEND PROPERTY APX_MODULES ${module})
+    set_property(GLOBAL APPEND PROPERTY APX_MODULES ${MODULE})
 
     if(INIT)
-        set_property(GLOBAL APPEND PROPERTY APX_MODULES_INIT ${module})
+        set_property(GLOBAL APPEND PROPERTY APX_MODULES_INIT ${MODULE})
     endif()
 
     set(meta "${CMAKE_CURRENT_SOURCE_DIR}/${MODULE_NAME}.yml")
     if(EXISTS ${meta})
         set_property(GLOBAL APPEND PROPERTY APX_MODULES_META ${meta})
         # message(STATUS "META: ${meta}")
+    else()
+        set(meta)
     endif()
 
     # glob SRCS when needed
     if(NOT SRCS)
-        if(INTERFACE)
-            set(SRCS "*.h*")
-        else()
-            set(SRCS "*.[ch]*")
-        endif()
+        set(SRCS "*.[ch]*")
     endif()
-    include(srcs)
+    set(srcs)
+    foreach(src ${SRCS})
+        if(NOT EXISTS ${src})
+            file(
+                GLOB src_exp
+                RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}
+                ${src}
+            )
+            if(src_exp)
+                set(src ${src_exp})
+            endif()
+        endif()
+        if(NOT src MATCHES "[\*]")
+            list(APPEND srcs ${src})
+        endif()
+    endforeach()
+    set(SRCS ${srcs})
+
+    # check if src contains headers only - i.e. interface
+    if(GENSRC)
+        foreach(gensrc ${GENSRC})
+            get_target_property(gensrc_targets ${gensrc} INTERFACE_SOURCES)
+            list(APPEND srcs ${gensrc_targets})
+        endforeach()
+    endif()
+    foreach(src ${srcs})
+        get_filename_component(ext ${src} LAST_EXT)
+        string(TOLOWER ${ext} ext)
+        if(ext MATCHES "^\.[cs]")
+            set(STATIC ON)
+        endif()
+    endforeach()
 
     # add library
-    if(INTERFACE)
-        add_library(${MODULE} INTERFACE ${srcs})
+    if(STATIC)
+        add_library(${MODULE} STATIC EXCLUDE_FROM_ALL ${SRCS})
     else()
-        add_library(${MODULE} STATIC EXCLUDE_FROM_ALL ${srcs})
+        add_library(${MODULE} INTERFACE ${SRCS})
     endif()
 
     # includes
@@ -89,16 +140,17 @@ function(apx_module)
                 file(CREATE_LINK ${inc} ${inc_link} SYMBOLIC)
                 set(inc ${CMAKE_CURRENT_BINARY_DIR})
             endif()
-            if(INTERFACE)
-                target_include_directories(${MODULE} INTERFACE ${inc})
-            else()
+            if(STATIC)
                 target_include_directories(${MODULE} PUBLIC ${inc})
+            else()
+                target_include_directories(${MODULE} INTERFACE ${inc})
             endif()
         endforeach()
     endif()
 
     # defines
-    if(NOT INTERFACE)
+    if(STATIC)
+        string(TOLOWER ${MODULE_NAME} MODULE_NAME_LC)
         target_compile_definitions(${MODULE} PRIVATE "MODULE_NAME=\"${MODULE_NAME_LC}\"")
     endif()
 
@@ -116,11 +168,11 @@ function(apx_module)
             add_dependencies(${MODULE} ${gensrc})
             get_target_property(gensrc_targets ${gensrc} INTERFACE_SOURCES)
             get_target_property(gensrc_dir ${gensrc} INCLUDE_DIRECTORIES)
-            if(INTERFACE)
-                target_include_directories(${MODULE} INTERFACE ${gensrc_dir})
-            else()
+            if(STATIC)
                 target_include_directories(${MODULE} PUBLIC ${gensrc_dir})
                 target_sources(${MODULE} PRIVATE ${gensrc_targets})
+            else()
+                target_include_directories(${MODULE} INTERFACE ${gensrc_dir})
             endif()
         endforeach()
     endif()
@@ -130,10 +182,10 @@ function(apx_module)
         foreach(dep ${DEPENDS})
             get_target_property(dep_type ${dep} TYPE)
             if(${dep_type} STREQUAL "STATIC_LIBRARY" OR ${dep_type} STREQUAL "INTERFACE_LIBRARY")
-                if(INTERFACE)
-                    target_link_libraries(${MODULE} INTERFACE ${dep})
-                else()
+                if(STATIC)
                     target_link_libraries(${MODULE} PUBLIC ${dep})
+                else()
+                    target_link_libraries(${MODULE} INTERFACE ${dep})
                 endif()
             else()
                 add_dependencies(${MODULE} ${dep})
@@ -145,6 +197,12 @@ function(apx_module)
     set(report ${MODULE})
     if(INIT)
         set(report "${report} INIT")
+    endif()
+    if(NOT STATIC)
+        set(report "${report} IFACE")
+    endif()
+    if(meta)
+        set(report "${report} META")
     endif()
     message(STATUS "MODULE: ${report}")
 
