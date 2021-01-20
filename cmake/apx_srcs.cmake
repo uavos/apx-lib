b4489fcc8fea68609ecc5523cde310aa2270ae00
Aliaksei Stratsilatau <sa@uavos.com>
2021-01-20 17:18:47 +0300

    src glob filters

--- cmake/apx_srcs.cmake
+++ cmake/apx_srcs.cmake
@@ -8,7 +8,16 @@ macro(apx_srcs var)
 
     set(srcs)
     foreach(src ${${var}})
-        if(src MATCHES "[\*]")
+        if(EXISTS "${src}/")
+            file(
+                GLOB_RECURSE src_exp
+                RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}
+                ${src}/*
+            )
+            if(src_exp)
+                set(src ${src_exp})
+            endif()
+        elseif(src MATCHES "[\\*]")
             file(
                 GLOB src_exp
                 RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}
@@ -18,9 +27,17 @@ macro(apx_srcs var)
                 set(src ${src_exp})
             endif()
         endif()
-        if(NOT src MATCHES "[\*]")
-            list(APPEND srcs ${src})
-        endif()
+        foreach(fsrc ${src})
+            if(fsrc MATCHES "[\\*]+")
+                continue()
+            endif()
+            get_filename_component(fname ${fsrc} NAME)
+            if(fname MATCHES "^[\\._].+")
+                # message(FATAL_ERROR ${fname})
+                continue()
+            endif()
+            list(APPEND srcs ${fsrc})
+        endforeach()
     endforeach()
 
     set(${var} ${srcs})
