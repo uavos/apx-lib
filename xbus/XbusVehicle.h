2cb8a4384e4a1900f64c709e19d2d678241a4c0b
Aliaksei Stratsilatau <sa@uavos.com>
2021-05-06 21:19:03 +0300

    xpdr fix

--- xbus/XbusVehicle.h
+++ xbus/XbusVehicle.h
@@ -55,7 +55,7 @@ typedef float lat_t;
 typedef float lon_t;
 typedef int16_t alt_t;
 typedef uint16_t speed_t;
-typedef int16_t course_t;
+typedef int16_t bearing_t;
 typedef uint8_t mode_t;
 
 struct xpdr_s
@@ -69,7 +69,7 @@ struct xpdr_s
 
     static inline uint16_t psize()
     {
-        return sizeof(lat_t) + sizeof(lon_t) + sizeof(alt_t) + sizeof(speed_t) + sizeof(course_t)
+        return sizeof(lat_t) + sizeof(lon_t) + sizeof(alt_t) + sizeof(speed_t) + sizeof(bearing_t)
                + sizeof(mode_t);
     }
 
@@ -79,7 +79,7 @@ struct xpdr_s
         *s >> lon;
         alt = s->read<alt_t, float>();
         speed = s->read<speed_t, float>() / 100.0f;
-        bearing = s->read<course_t, float>() / (32768.0f / 180.0f);
+        bearing = s->read<bearing_t, float>() / (32768.0f / 180.0f);
         *s >> mode;
     }
     inline void write(XbusStreamWriter *s) const
@@ -88,7 +88,7 @@ struct xpdr_s
         *s << lon;
         s->write<alt_t, float>(alt);
         s->write<speed_t, float>(speed * 100.0f);
-        s->write<course_t, float>(bearing * (32768.0f / 180.0f));
+        s->write<bearing_t, float>(bearing * (32768.0f / 180.0f));
         *s << mode;
     }
 };
